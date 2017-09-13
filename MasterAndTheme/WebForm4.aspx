cb1d9b2b761342394a8313265f3ffb486f756f4c
Andreas Schultz <andreasschultz@hotmail.com>
2017-09-13 15:29:05 +0200

    Initial commit

---
+++ MasterAndTheme/WebForm4.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm4.aspx.cs" Inherits="MasterAndTheme.WebForm4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Database connection</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" EmptyDataText="No data">
+            </asp:GridView>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SHAZAM!" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MasterAndTheme/WebForm4.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MasterAndTheme
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"data source = .\sqlexpress; integrated security = true; database = Northwind");
+        SqlCommand cmd = null;
+        SqlDataReader rdr = null;
+        string sqlsel = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+            Label1.Text = "No error message";
+        }
+
+        public void ShowMyData()
+        {
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand(sqlsel, conn);
+
+                rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+            }
+
+            catch(Exception ex)
+            {
+                Label1.Text = ex.Message;
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            sqlsel = "Select top 10 * from products";
+            ShowMyData();
+        }
+    }
+}
